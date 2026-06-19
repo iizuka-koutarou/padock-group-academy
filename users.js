70ef85ab9c353514b1eec672e6e99a1454f65857
iizuka-koutarou <ittyan007@gmail.com>
2026-06-19 20:35:22 +0900

    Create users.js

---
+++ users.js
@@ -0,0 +1,29 @@
+// users.js
+
+const users = {
+
+admin:{
+password:"admin2026",
+role:"admin",
+name:"管理者"
+},
+
+iizuka_k:{
+password:"1234",
+role:"user",
+name:"飯塚康太郎"
+},
+
+iizuka_s:{
+password:"1234",
+role:"user",
+name:"飯塚宗一郎"
+},
+
+aoshima:{
+password:"1234",
+role:"user",
+name:"青島昇平"
+}
+
+};
